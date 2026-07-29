fa4186ea2018ebd1001e88c658b06e3a70a4abfe
smarchena <santy832@gmail.com>
2026-07-29 14:28:12 -0500

    feat: initialize a new project with essential configuration files and structures

---
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ProductCard } from '@products/components/product-card/product-card';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProductCard],
+  templateUrl: './home-page.html',
+})
+export class HomePage {}
