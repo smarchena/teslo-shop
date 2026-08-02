5fcf871684817c93c48b0e3880b526e121b6784e
smarchena <santy832@gmail.com>
2026-08-02 14:16:14 -0500

    feat: implement ProductService and integrate with HomePage component

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home-page',
   imports: [ProductCard],
   templateUrl: './home-page.html',
 })
-export class HomePage {}
+
+export class HomePage {
+  productsService = inject(ProductService)
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ( {params} ) => {
+      return this.productsService.getProducts()
+    }
+  })
+
+}
