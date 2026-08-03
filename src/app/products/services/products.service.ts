984428b8cda0de0c485605951cd0f527a23964bb
smarchena <santy832@gmail.com>
2026-08-02 20:46:15 -0500

    feat: enhance ProductService to support pagination and filtering by gender

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -2,14 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ProductsResponse } from '@products/interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+const baseUrl = environment.baseUrl
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
   private http = inject(HttpClient)
 
 
-  getProducts(): Observable<ProductsResponse> {
-    return this.http.get<ProductsResponse>('http://localhost:3000/api/products')
+  getProducts(options: Options): Observable<ProductsResponse> {
+
+    const {limit = 9, offset = 0, gender = ''} = options
+
+    return this.http.get<ProductsResponse>(`${baseUrl}/products`,  {
+      params: {
+        limit: limit,
+        offset: offset,
+        gender: gender
+      }
+
+    }
+    )
     .pipe(
       tap( resp => console.log(resp) )
     )
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -15,7 +15,10 @@ export class HomePage {
   productsResource = rxResource({
     params: () => ({}),
     stream: ( {params} ) => {
-      return this.productsService.getProducts()
+      return this.productsService.getProducts({
+        limit: 1,
+        gender: 'women'
+      })
     }
   })
 
