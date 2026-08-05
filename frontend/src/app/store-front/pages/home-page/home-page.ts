82cd4069526751755c50d95b60f0b8ea0ece21b9
smarchena <santy832@gmail.com>
2026-08-05 16:51:18 -0500

    feat: enhance pagination component with active page tracking and update home page to reflect current page

--- frontend/src/app/store-front/pages/home-page/home-page.ts
+++ frontend/src/app/store-front/pages/home-page/home-page.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { ProductService } from '@products/services/products.service';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { Pagination } from '@shared/components/pagination/pagination';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -12,11 +14,24 @@ import { Pagination } from '@shared/components/pagination/pagination';
 
 export class HomePage {
   productsService = inject(ProductService)
+  activaredRoute = inject(ActivatedRoute)
+
+  currentPage = toSignal(
+    this.activaredRoute.queryParamMap.pipe(
+      map(params => params.get('page') ? +params.get('page')! : 1),
+      map(page => (isNaN(page) ? 1 : page))
+    ),
+    {
+      initialValue: 1
+    }
+  )
 
   productsResource = rxResource({
-    params: () => ({}),
-    stream: ( {params} ) => {
-      return this.productsService.getProducts({})
+    params: () => ({page: this.currentPage() - 1}), //se resta para que la ultima pagina no salga vacía, vamos de 9 en 9
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        offset: params.page * 9
+      })
     }
   })
 
