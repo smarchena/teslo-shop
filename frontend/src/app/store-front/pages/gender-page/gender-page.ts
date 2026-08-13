d7e7b614d56ab5c321d39bc08a83c6c587b9aba1
smarchena <santy832@gmail.com>
2026-08-13 12:51:47 -0500

    feat: implement pagination service and integrate it into gender and home pages

--- frontend/src/app/store-front/pages/gender-page/gender-page.ts
+++ frontend/src/app/store-front/pages/gender-page/gender-page.ts
@@ -4,27 +4,45 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '@products/services/products.service';
 import { map } from 'rxjs';
 import { ProductCard } from "@products/components/product-card/product-card";
+import { Pagination } from "@shared/components/pagination/pagination";
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
   templateUrl: './gender-page.html',
 })
 export class GenderPage {
 
   route = inject(ActivatedRoute)
   productsService = inject(ProductService)
+  PaginationService = inject(PaginationService)
+
 
   gender = toSignal(this.route.params.pipe(
     map(({gender}) => gender)
   ))
 
+  //   currentPage = toSignal(
+  //   this.activaredRoute.queryParamMap.pipe(
+  //     map(params => params.get('page') ? +params.get('page')! : 1),
+  //     map(page => (isNaN(page) ? 1 : page))
+  //   ),
+  //   {
+  //     initialValue: 1
+  //   }
+  // )
+
   productsResource = rxResource({
-    params: () => ({
+    params: () => ({ page: this.PaginationService.currentPage() - 1,
       gender: this.gender()
     }),
     stream: ( {params} ) => {
-      return this.productsService.getProducts({gender: params.gender})
+      return this.productsService.getProducts({
+        gender: params.gender,
+        offset: params.page * 9
+      })
     }
   })
 }
--- frontend/src/app/store-front/pages/home-page/home-page.ts
+++ frontend/src/app/store-front/pages/home-page/home-page.ts
@@ -5,6 +5,7 @@ import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { Pagination } from '@shared/components/pagination/pagination';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
@@ -14,20 +15,22 @@ import { map } from 'rxjs';
 
 export class HomePage {
   productsService = inject(ProductService)
-  activaredRoute = inject(ActivatedRoute)
+  paginationService = inject(PaginationService)
 
-  currentPage = toSignal(
-    this.activaredRoute.queryParamMap.pipe(
-      map(params => params.get('page') ? +params.get('page')! : 1),
-      map(page => (isNaN(page) ? 1 : page))
-    ),
-    {
-      initialValue: 1
-    }
-  )
+  // activaredRoute = inject(ActivatedRoute)
+
+  // currentPage = toSignal(
+  //   this.activaredRoute.queryParamMap.pipe(
+  //     map(params => params.get('page') ? +params.get('page')! : 1),
+  //     map(page => (isNaN(page) ? 1 : page))
+  //   ),
+  //   {
+  //     initialValue: 1
+  //   }
+  // )
 
   productsResource = rxResource({
-    params: () => ({page: this.currentPage() - 1}), //se resta para que la ultima pagina no salga vacía, vamos de 9 en 9
+    params: () => ({page: this.paginationService.currentPage() - 1}), //se resta para que la ultima pagina no salga vacía, vamos de 9 en 9
     stream: ({ params }) => {
       return this.productsService.getProducts({
         offset: params.page * 9
