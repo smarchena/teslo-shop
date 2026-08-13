0251e1ee2e4e65ce8de9077c75437b1c74b0156a
smarchena <santy832@gmail.com>
2026-08-13 14:06:43 -0500

    feat: add authentication module with login and registration pages, and configure routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    //toDo: Guards
+  },
+
   {
     path: '',
     loadChildren: () => import('./store-front/store-front.routes')
-  }
+  },
+
 ];
