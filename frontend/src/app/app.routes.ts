a8f78f73371f3052b25cdcf4aae8ec216377e387
smarchena <santy832@gmail.com>
2026-08-13 18:31:48 -0500

    feat: implement NotAuthenticatedGuard for route protection

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated-guard';
 
 export const routes: Routes = [
 
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
-    //toDo: Guards
+    canMatch: [
+      NotAuthenticatedGuard
+    ]
   },
 
   {
