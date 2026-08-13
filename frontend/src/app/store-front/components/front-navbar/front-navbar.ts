3fe03e56a11906bce372ad73e847b84885a88eb8
smarchena <santy832@gmail.com>
2026-08-13 16:45:06 -0500

    feat: update FrontNavbar component to display user info and logout button based on authentication status

--- frontend/src/app/store-front/components/front-navbar/front-navbar.ts
+++ frontend/src/app/store-front/components/front-navbar/front-navbar.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'front-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-navbar.html',
 })
-export class FrontNavbar { }
+export class FrontNavbar {
+
+  authService = inject(AuthService)
+}
