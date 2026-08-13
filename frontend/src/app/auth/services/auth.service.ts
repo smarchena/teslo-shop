31f0122990a3bb93b815af98173e0b56a6d4e145
smarchena <santy832@gmail.com>
2026-08-13 18:05:20 -0500

    feat: add logging and auth interceptors for improved request handling

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -15,7 +15,7 @@ const baseUrl = environment.baseUrl
 export class AuthService {
   private _authStatus = signal<AuthStatus>('checking')
   private _user = signal<User | null>(null)
-  private _token = signal<string | null>(null)
+  private _token = signal<string | null>(localStorage.getItem('token'))
 
   private http = inject(HttpClient)
 
@@ -54,9 +54,9 @@ export class AuthService {
     }
 
     return this.http.get<AuthResponse>(`${baseUrl}/auth/check-status`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
+      // headers: {
+      //   Authorization: `Bearer ${token}`
+      // }
     }).pipe(
       map(resp => this.handleAuthSuccess(resp)),
       catchError((error: any) => this.handleAuthError(error))
@@ -68,7 +68,8 @@ export class AuthService {
     this._token.set(null)
     this._authStatus.set('not-aunthenticated')
 
-    localStorage.removeItem('token')
+    //ToDO: Revertir
+    // localStorage.removeItem('token')
   }
 
   private handleAuthSuccess({ token, user }: AuthResponse) {
