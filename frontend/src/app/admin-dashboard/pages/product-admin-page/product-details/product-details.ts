56b804c1e9c4b8233347dd8407670226c99f5b60
smarchena <santy832@gmail.com>
2026-08-15 14:01:33 -0500

    feat: enhance gender selection buttons and implement size selection logic in product details component

--- frontend/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ frontend/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -42,6 +42,18 @@ export class ProductDetails implements OnInit {
 
   }
 
+  onSizeClicked(size: string) {
+    const currentSizes = this.productForm.value.sizes ?? []
+
+    if (currentSizes.includes(size)) {
+      currentSizes.splice(currentSizes.indexOf(size), 1)
+    } else {
+      currentSizes.push(size)
+    }
+
+    this.productForm.patchValue({sizes: currentSizes})
+
+  }
 
   onSubmit() {
     console.log(this.productForm.value)
