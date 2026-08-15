334e49eda0fbc8099ee444addd9f46243c11da43
smarchena <santy832@gmail.com>
2026-08-15 12:50:23 -0500

    feat: enhance product details form with reactive forms and validation

--- frontend/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ frontend/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -1,15 +1,38 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '@products/interfaces/product.interface';
 import { ProductCarousel } from "@products/components/product-carousel/product-carousel";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
+import { AuthService } from '@auth/services/auth.service';
+
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarousel],
+  imports: [ProductCarousel, ReactiveFormsModule],
   templateUrl: './product-details.html',
 })
 export class ProductDetails {
-
   product = input.required<Product>()
 
+
+  fb = inject(FormBuilder)
+
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]]
+  })
+
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+
+
+  onSubmit() {
+    console.log(this.productForm.value)
+  }
 }
