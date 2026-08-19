b5a524914f788bd0eeb6e04afcab49f3c72be919
smarchena <santy832@gmail.com>
2026-08-19 13:43:19 -0500

    feat: update app configuration and environment settings for production

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -10,7 +10,7 @@ import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(
       withFetch(),
       withInterceptors([
